b129aece02a87effe7afb7537815b219c07db56a
jonimurray <jonifacu52@hotmail.com>
2022-11-11 17:23:31 -0300

    Proyectoo

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Harina",
+        precio: 190,
+        cantidad: 1,
+        img:
+            "https://carrefourar.vtexassets.com/arquivos/ids/205215/7792180139313_02.jpg?v=637601556417500000",
+    },
+    {
+        id: 2,
+        nombre: "Galletitas",
+        precio: 270,
+        cantidad: 1,
+        img:
+            "https://jumboargentina.vtexassets.com/arquivos/ids/712811/Galletitas-Oreo-354gr-1-887492.jpg?v=637946193909070000",
+    },
+    {
+        id: 3,
+        nombre: "cerveza",
+        precio: 360,
+        cantidad: 1,
+        img:
+            "http://d2r9epyceweg5n.cloudfront.net/stores/001/354/614/products/cerveza-corona-extra-710ml_grande1-954ac3354037eab19016033858459274-640-0.png"
+    },
+    {
+        id: 4,
+        nombre: "Leche",
+        precio: 210,
+        cantidad: 1,
+        img:
+            "https://almacenfamily.com/productos/leche-laserenisima-1-lv.png"
+    },
+];
